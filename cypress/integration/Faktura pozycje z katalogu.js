ad78e23f1e67b8f280ba9fc9cf690335af01a429
DESKTOP-6ONQI9O\barte <bartek.lazarowicz@wfirma.pl>
2017-12-28 12:00:32 +0100

    Start
    Signed-off-by: DESKTOP-6ONQI9O\barte <bartek.lazarowicz@wfirma.pl>

--- cypress/integration/Faktura pozycje z katalogu.js
+++ cypress/integration/Faktura pozycje z katalogu.js
@@ -11,21 +11,44 @@ describe('Dokumenty sprzedaży', function () {
         //wejdz na beta.wfirma.pl
         cy.visit( Site + '/users/login')
         //zaloguj sie
-        cy.get('input#UserLogin').type(Login).get('input#UserPassword').type(Password).get('button.btn.btn-lg.btn-primary.btn-block.btn-submit').click()
+        cy.get('input#UserLogin')
+            .type(Login)
+            .get('input#UserPassword')
+            .type(Password)
+            .get('button.btn.btn-lg.btn-primary.btn-block.btn-submit')
+            .click()
         //wybierz firme
         cy.visit( Site + '/user_companies/login/' + CompanyID)
         //przejdz do Przychody
         cy.visit( Site + '/invoices/index/all')
         // klik w Wystaw
-        cy.get('span.menu-label:first').click()
+        cy.get('span.menu-label:first')
+            .click()
         //uzupełnianie danych kontrahenta
-        cy.get('input[name="data[ContractorDetail][name]"]').type('kontrahent' + RandomNumbContractor).get('label').contains('Nabywca').click()
-            .get('input[name="data[ContractorDetail][zip]"]').type(RandomNumbContractor) //.get('input[name="data[ContractorDetail][zip]"]').contains(RandomNumbContractor)
-            .get('input[name="data[ContractorDetail][city]"]').clear().type('Miasto ' + RandomNumbContractor).get('input[name="data[ContractorDetail][city]"]').should('have.value', 'Miasto ' + RandomNumbContractor)
-            .get('input[name="data[ContractorDetail][street]"]').type('Ulica ' + RandomNumbContractor).get('input[name="data[ContractorDetail][street]"]').should('have.value', 'Ulica ' + RandomNumbContractor)
-            .get('label div.input-append.input-prepend.inputer.input-select:first').click()
-            .get('span').contains('Inny').click()
-            .get('input[name="data[ContractorDetail][nip]"]').type(RandomNumbContractor).get('input[name="data[ContractorDetail][nip]"]').should('have.value','' + RandomNumbContractor)
+        cy.get('input[name="data[ContractorDetail][name]"]')
+            .type('kontrahent' + RandomNumbContractor)
+            .get('label').contains('Nabywca')
+            .click()
+            .get('input[name="data[ContractorDetail][zip]"]')
+            .type(RandomNumbContractor) //.get('input[name="data[ContractorDetail][zip]"]').contains(RandomNumbContractor)
+            .get('input[name="data[ContractorDetail][city]"]')
+            .clear()
+            .type('Miasto ' + RandomNumbContractor)
+            .get('input[name="data[ContractorDetail][city]"]')
+                .should('have.value', 'Miasto ' + RandomNumbContractor)
+            .get('input[name="data[ContractorDetail][street]"]')
+            .type('Ulica ' + RandomNumbContractor)
+            .get('input[name="data[ContractorDetail][street]"]')
+                .should('have.value', 'Ulica ' + RandomNumbContractor)
+            .get('label div.input-append.input-prepend.inputer.input-select:first')
+            .click()
+            .get('span')
+            .contains('Inny')
+            .click()
+            .get('input[name="data[ContractorDetail][nip]"]')
+            .type(RandomNumbContractor)
+            .get('input[name="data[ContractorDetail][nip]"]')
+                .should('have.value','' + RandomNumbContractor)
 
 
         //dodawanie produktu z kartoteki
--- cypress/integration/rejestracja.js
+++ cypress/integration/rejestracja.js
@@ -1,19 +1,30 @@
 describe('Rejestracja Wfirmy', function () {
     it('Register_Steps', function () {
-        cy.visit('https://beta.wfirma.pl')
+        var Site = 'https://beta.wfirma.pl'
+        var Login = 'bartek.lazarowicz+'+Math.floor(Math.random() * 10001)+'@wfirma.pl'
+        var NIP = '9151677484'
+        var Password = '123123qwe'
+        var CompanyName = 'BUSINESS TAX Sp. z o. o.'
+        var Street = 'ul. Bolesława Krzywoustego'
+        var BuildingNumber = '105'
+        var FlatNumber = '21'
+        var Zip = '51-166'
+        var City = 'Wrocław'
+
+        cy.visit(Site)
         cy.get('a.btn.btn-primary.btn-arrow').should('contain','darmo')
         cy.get('a.btn.btn-colored.btn-register').click()
-        cy.get('input#UserLogin').type('bartek.lazarowicz+'+Math.floor(Math.random() * 10001)+'@wfirma.pl')
-        cy.get('input#UserPassword').type('123123qwe')
-        cy.get('input#CompanyNip').type('9151677484')
+        cy.get('input#UserLogin').type(Login)
+        cy.get('input#UserPassword').type(Password)
+        cy.get('input#CompanyNip').type(NIP)
         cy.get('div.checkbox0').click()
         cy.get('button.btn-submit').click()
-        cy.get('input#CompanyName').should('have.value','BUSINESS TAX Sp. z o. o.')
-        cy.get('input#CompanyAddressMainStreet').should('have.value','Bolesława Krzywoustego')
-        cy.get('input#CompanyAddressMainBuildingNumber').should('have.value','105')
-        cy.get('input#CompanyAddressMainFlatNumber').should('have.value','21')
-        cy.get('input#CompanyAddressMainZip').should('have.value','51-166')
-        cy.get('input#CompanyAddressMainCity ').should('have.value','Wrocław')
+        cy.get('input#CompanyName').should('have.value',CompanyName)
+        cy.get('input#CompanyAddressMainStreet').should('have.value',Street)
+        cy.get('input#CompanyAddressMainBuildingNumber').should('have.value',BuildingNumber)
+        cy.get('input#CompanyAddressMainFlatNumber').should('have.value',FlatNumber)
+        cy.get('input#CompanyAddressMainZip').should('have.value',Zip)
+        cy.get('input#CompanyAddressMainCity ').should('have.value',City)
         cy.get('button.btn-submit').click()
         cy.get('div.check-box.fa.fa-check.large.checkbox2').click()
         cy.get('a.btn-submit').click()
