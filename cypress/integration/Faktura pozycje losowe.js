b212250a5bc22fefc7e3c4cd0a3babdf365f46d9
Bartłomiej Lazarowicz <bartek.lazarowicz@wfirma.pl>
2017-12-28 20:23:14 +0100

    segregacja

--- cypress/integration/Faktura pozycje losowe.js
+++ cypress/integration/Faktura pozycje losowe.js
@@ -12,7 +12,12 @@ describe('Dokumenty sprzedaży', function () {
         //wejdz na beta.wfirma.pl
         cy.visit( Site + '/users/login')
         //zaloguj sie
-        cy.get('input#UserLogin').type(Login).get('input#UserPassword').type(Password).get('button.btn.btn-lg.btn-primary.btn-block.btn-submit').click()
+        cy.get('input#UserLogin')
+            .type(Login)
+            .get('input#UserPassword')
+            .type(Password)
+            .get('button.btn.btn-lg.btn-primary.btn-block.btn-submit')
+            .click()
         //wybierz firme
         cy.visit( Site + '/user_companies/login/' + CompanyID)
         //przejdz do Przychody
@@ -20,26 +25,50 @@ describe('Dokumenty sprzedaży', function () {
         // klik w Wystaw
         cy.get('span.menu-label:first').click()
         //uzupełnianie danych kontrahenta
-        cy.get(DataInput + '[ContractorDetail][name]"]').type('kontrahent' + RandomNumbContractor).get('label').contains('Nabywca').click()
-            .get(DataInput + '[ContractorDetail][zip]"]').type(RandomNumbContractor) //.get(DataInput + '[ContractorDetail][zip]"]').contains(RandomNumbContractor)
-            .get(DataInput + '[ContractorDetail][city]"]').clear().type('Miasto ' + RandomNumbContractor).get(DataInput + '[ContractorDetail][city]"]').should('have.value', 'Miasto ' + RandomNumbContractor)
-            .get(DataInput + '[ContractorDetail][street]"]').type('Ulica ' + RandomNumbContractor).get(DataInput + '[ContractorDetail][street]"]').should('have.value', 'Ulica ' + RandomNumbContractor)
-            .get('label div.input-append.input-prepend.inputer.input-select:first').click()
-            .get('span').contains('Inny').click()
-            .get(DataInput + '[ContractorDetail][nip]"]').type(RandomNumbContractor).get(DataInput + '[ContractorDetail][nip]"]').should('have.value','' + RandomNumbContractor)
+        cy.get(DataInput + '[ContractorDetail][name]"]')
+            .type('kontrahent' + RandomNumbContractor)
+            .get('label').contains('Nabywca')
+            .click()
+            .get(DataInput + '[ContractorDetail][zip]"]')
+            .type(RandomNumbContractor)
+            //.get(DataInput + '[ContractorDetail][zip]"]').contains(RandomNumbContractor)
+            .get(DataInput + '[ContractorDetail][city]"]')
+            .clear()
+            .type('Miasto ' + RandomNumbContractor)
+            .get(DataInput + '[ContractorDetail][city]"]')
+            .should('have.value', 'Miasto ' + RandomNumbContractor)
+            .get(DataInput + '[ContractorDetail][street]"]')
+            .type('Ulica ' + RandomNumbContractor)
+            .get(DataInput + '[ContractorDetail][street]"]')
+            .should('have.value', 'Ulica ' + RandomNumbContractor)
+            .get('label div.input-append.input-prepend.inputer.input-select:first')
+            .click()
+            .get('span')
+            .contains('Inny').click()
+            .get(DataInput + '[ContractorDetail][nip]"]')
+            .type(RandomNumbContractor)
+            .get(DataInput + '[ContractorDetail][nip]"]')
+            .should('have.value','' + RandomNumbContractor)
         //dodawanie określonej ilości pozycji z produktami
         for (var PositionIndex=0; PositionIndex < InvoiceContentCount; ) {
             var RandomNumb = (Math.floor(Math.random() * 10001)) //liczby losowe dla pozycji
             //uzupełnianie danych produktu
-            cy.get(DataInput + '[InvoiceContent][' + PositionIndex + '][name]"]').type(RandomNumb)
-                .get(DataInput + '[InvoiceContent][' + PositionIndex + '][count]"]').type(RandomNumb)
-                .get(DataInput + '[InvoiceContent][' + PositionIndex + '][price]"]').type(RandomNumb)
-                .get('label').contains('Nabywca').click()
+            cy.get(DataInput + '[InvoiceContent][' + PositionIndex + '][name]"]')
+                .type(RandomNumb)
+                .get(DataInput + '[InvoiceContent][' + PositionIndex + '][count]"]')
+                .type(RandomNumb)
+                .get(DataInput + '[InvoiceContent][' + PositionIndex + '][price]"]')
+                .type(RandomNumb)
+                .get('label')
+                .contains('Nabywca')
+                .click()
             //dodawanie nowej pozycji
-            cy.get('a.btn.btn-mini.add-row').click()
+            cy.get('a.btn.btn-mini.add-row')
+                .click()
             PositionIndex++;
         }
         //akceptacja formularza
-        cy.get('div.dialogbox-content form').submit()
+        cy.get('div.dialogbox-content form')
+            .submit()
     });
 });
